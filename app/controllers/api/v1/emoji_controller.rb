e03b2b2adf8f5af3cbef68f3b731f2687a11d906
Caisr <madaocai@gmail.com>
2022-08-15 01:06:20 +0000

    update: 修改emoji.json 存放目录

--- app/controllers/api/v1/emoji_controller.rb
+++ app/controllers/api/v1/emoji_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::EmojiController < ApplicationController
   def index
-    @emoji = File.read("#{Rails.root}/public/emoji_v14.0.json")
+    @emoji = File.read("#{Rails.root}/static/emoji_v14.0.json")
     if stale?(@emoji)
       expires_in 3.months
       send_response(JSON.parse(@emoji))
